9737d8e7e3cefd7593ee77f213fef6fc102404e4
Alex Coleman <a.coleman1@leeds.ac.uk>
2020-03-17 11:12:36 +0000

    updated files to include loading miniconda

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,20 @@ wget https://julialang-s3.julialang.org/bin/linux/x64/1.3/julia-1.3.1-linux-x86_
 tar -xzf julia-1.3.1-linux-x86\_64.tar.gz
 
 echo 'export PATH=$PATH:/home/vagrant/Downloads/julia-1.3.1/bin' >> /etc/profile
+
+# return to home dir
+cd /home/vagrant/
+
+## install miniconda 
+
+apt-get update
+apt-get install git
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b -p /home/vagrant/miniconda
+# ensures conda loaded in shell
+echo "source /home/vagrant/miniconda/etc/profile.d/conda.sh" >> /etc/profile
+source /home/vagrant/miniconda/etc/profile.d/conda.sh
+
+## create an environment for jupyter notebooks
+conda env create -f environment.yml
